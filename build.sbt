e4aaddd9420a58d482af529c03cd8fd3e65f6baa
m.basij6962 <mohamadbiiij@gmail.com>
2022-01-15 16:54:56 +0330

    task 3 has been completed!

--- build.sbt
+++ build.sbt
@@ -1,12 +1,16 @@
-name := "Verve"
+name := "Verve_Task"
 
 version := "0.1"
 
-scalaVersion := "2.13.7"
+scalaVersion := "2.13.1"
 
 
 libraryDependencies += "com.lihaoyi" %% "upickle" % "1.4.3"
 libraryDependencies += "com.lihaoyi" %% "os-lib" % "0.8.0"
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
+//libraryDependencies += "org.json4s" %% "json4s-native" % "4.0.3"
+//libraryDependencies += "org.json4s" %% "json4s-jackson" % "4.0.3"
+//libraryDependencies += "org.scala-lang" % "scala-parser-combinators" % "2.11.0-M4"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.0"
 libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.3.5"
+
 libraryDependencies += "org.apache.spark" %% "spark-core" % "3.2.0"
--- src/main/scala/com/verve/challenge/spark/Task3.scala
+++ src/main/scala/com/verve/challenge/spark/Task3.scala
@@ -1,74 +1,90 @@
-//package com.verve.task1_2.spark
-//
-//
-//import org.apache.spark.sql.SparkSession
-//import org.apache.spark.sql.types._
-//
-//object Task3 {
-//
-//  def main(args: Array[String]): Unit = {
-//
-//    println("Start ... ")
-//    System.setProperty("hadoop.home.dir", "C:\\Users\\m.basij\\Documents\\winutils-master\\hadoop-2.7.1\\bin")
-//
-//    val spark = SparkSession.builder().appName("Verve").master("local[*]").getOrCreate()
-//
-//    //    val schema = new StructType()
-//    //      .add("app_id", StringType, true)
-//    //      .add("advertiser_id", StringType, true)
-//    //      .add("country_code", StringType, true)
-//    //      .add("id", StringType, true)
-//
-//    val impressionsDF = spark.read
-//      //      .schema(schema)
-//      .option("inferSchema", "true")
-//      .option("header", "true")
-//      .option("multiline", "true")
-//      .json("C:\\Users\\m.basij\\IdeaProjects\\Verve_Task\\src\\test\\resources\\impressions.json")
-//
-////    println("Schema impressionsDF")
-////    impressionsDF.printSchema()
-////
-////
-////    println("Show impressionsDF")
-////
-////    impressionsDF.show(false)
-//
-//
-//    val clicksDF = spark.read
-//      //      .schema(schema)
-//      .option("inferSchema", "true")
-//      .option("header", "true")
-//      .option("multiline", "true")
-//      .json("C:\\Users\\m.basij\\IdeaProjects\\Verve_Task\\src\\test\\resources\\clicks.json")
-//
-//    println("Schema clicksDF")
-////    clicksDF.printSchema()
-//    println("Show clicksDF")
-////    clicksDF.show(false)
-//
-//    impressionsDF.createOrReplaceTempView("impressionDF")
-//    clicksDF.createOrReplaceTempView("clicksDF")
-//
-////    /*val joinedDF =*/ impressionsDF.join(clicksDF,impressionsDF("id") ===  clicksDF("impression_id"),"fullouter")
-////      .select("app_id","impression_id","advertiser_id","country_code","revenue")
-//////      .groupBy("app_id","country_code","advertiser_id")
-//////      .count()
-//////      .sort()
-////      .show(100000,false)
-//
-//    spark.sql("select impressionDF.app_id, impressionDF.country_code,impressionDF.advertiser_id, sum(clicksDF.revenue) as revenue from clicksDF" +
-//      "  join impressionDF on clicksDF.impression_id = impressionDF.id" +
-//      " group by impressionDF.app_id, impressionDF.country_code,impressionDF.advertiser_id order by revenue desc").show(10000,false)
-//
-////    spark.sql("select sum(clicksDF.revenue) from clicksDF").show()
-//
-//
-////    spark.sql("select distinct(impressionDF.app_id), distinct(impressionDF.country_code) from impressionDF").show(10000,false)
-//
-//
-//    println("Joined DFs ...")
-//
-//    println("End ...")
-//  }
-//}
+package com.verve.task1_2.spark
+
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, concat_ws, expr, struct}
+
+
+object Task3 {
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    println("Start ... ")
+    System.setProperty("hadoop.home.dir", "C:\\Users\\m.basij\\Documents\\winutils-master\\hadoop-2.7.1\\bin")
+
+    val spark = SparkSession.builder().appName("Verve").master("local[*]").getOrCreate()
+
+
+    val impressionsDF = spark.read
+      //      .schema(schema)
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .option("multiline", "true")
+      .json("C:\\Users\\m.basij\\IdeaProjects\\Verve_Task\\src\\test\\resources\\impressions.json")
+
+
+    val clicksDF = spark.read
+      //      .schema(schema)
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .option("multiline", "true")
+      .json("C:\\Users\\m.basij\\IdeaProjects\\Verve_Task\\src\\test\\resources\\clicks.json")
+
+
+    impressionsDF.createOrReplaceTempView("impressionDF")
+    clicksDF.createOrReplaceTempView("clicksDF")
+
+
+    println("Starting pipeline  ...")
+
+
+    val joinedDF =
+      impressionsDF.join(clicksDF, impressionsDF("id") === clicksDF("impression_id"), "left").distinct()
+        .select("advertiser_id", "app_id", "country_code", "id", "revenue")
+
+    joinedDF.createOrReplaceTempView("joinedDF")
+
+
+    val df_sum_revenue = spark.sql("select joinedDF.advertiser_id,joinedDF.app_id ,joinedDF.country_code ,joinedDF.id, sum(joinedDF.revenue) as revenue " +
+      "from joinedDF " +
+      "group by joinedDF.advertiser_id,joinedDF.app_id ,joinedDF.country_code,joinedDF.id ").distinct()
+    //    df_sum_revenue.show(100000,false)
+
+    //    val cols1 = List("revenue")
+    //    val sums1 = cols1.map(colName => functions.sum(colName).cast("double").as("sum_" + colName))
+    //    df_sum_revenue.groupBy().agg(sums1.head, sums1.tail: _*).show()
+
+
+    df_sum_revenue.createOrReplaceTempView("dfWithRevenue")
+
+    val df_count_id = spark.sql("select impressionDF.advertiser_id , impressionDF.app_id, impressionDF.country_code, impressionDF.id ,count(impressionDF.id) as impressions " +
+      "from impressionDF " +
+      "group by impressionDF.advertiser_id, impressionDF.country_code,impressionDF.app_id, impressionDF.id").distinct()
+
+    df_count_id.createOrReplaceTempView("dfWithImpression")
+
+
+    val mergedDF = spark.sql("select dfWithRevenue.advertiser_id, dfWithRevenue.app_id, dfWithRevenue.country_code, " +
+      " dfWithRevenue.revenue / dfWithImpression.impressions as revenuePerImpression " +
+      "from dfWithRevenue inner join dfWithImpression on dfWithRevenue.id = dfWithImpression.id").distinct()
+
+
+    mergedDF.createOrReplaceTempView("mergedDF")
+
+
+    val df = spark.sql("select *" +
+      //      " listagg(x.advertiser_id,',') WITHIN GROUP (order by x.revenuePerImpression) " +
+      " from (select * from (select mergedDF.advertiser_id,mergedDF.app_id, mergedDF.country_code,mergedDF.revenuePerImpression,row_number() over (partition by mergedDF.app_id, mergedDF.country_code order by nvl(mergedDF.revenuePerImpression,0) desc) num_ from mergedDF) where num_ <= 5 order by num_ desc) ")
+
+
+    val ds = df.withColumn("advertiser_ids", struct(col("advertiser_id"), col("app_id"), col("country_code"),
+      col("revenuePerImpression").cast("double"), col("num_").cast("Int")))
+      .groupBy("app_id", "country_code")
+      .agg(concat_ws("-", expr("sort_array(collect_list(advertiser_ids)).advertiser_id")).alias("recommended_advertiser_ids"))
+
+    ds.show(100, false)
+
+    println("End ...")
+  }
+}
